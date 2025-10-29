b7d6c750880c68ac2b2ac0d6865f13e62e72068b
Karthik-S-codes <karthis3477@gmail.com>
2025-10-29 16:04:49 +0530

    new commit

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-
-    public static void main(String[] args){
-        static.out.println("Hello World");
+// HelloWorld.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
+}
